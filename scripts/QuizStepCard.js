8321bc174df4567027348c2cff78c5feb85134bb
Aleks-JS <kresnikov@yandex.ru>
2021-10-19 23:58:43 +0300

    refactoring

--- scripts/QuizStepCard.js
+++ scripts/QuizStepCard.js
@@ -1,53 +1,43 @@
-import {
-    APP
-} from "./variables.js";
+import {APP} from "./variables.js";
 import Answer from "./Answer.js";
+
 class QuizStepCard {
-    constructor(state) {
-        this.STATE = state;
-    }
+	constructor(state) {
+		this.STATE = state;
+	}
 
-    nextStep() {
-        this.STATE.currentStep += 1;
-        this.renderCard();
-    }
+	addAnswerItem(type, key, value) {
+		const label = document.createElement('label');
+		label.classList.add('list-group-item');
+		const answer = document.createElement('input');
+		answer.type = type;
+		answer.name = 'answer';
+		answer.classList.add('form-check-input', 'me-1');
+		answer.value = key;
+		answer.id = key;
+		if (type === 'radio') {
+			answer.required = true
+		}
+		label.textContent = value;
+		label.prepend(answer);
+		return label;
+	}
 
-    addAnswerItem(type, key, value) {
-        const label = document.createElement('label');
-        label.classList.add('list-group-item');
-        const answer = document.createElement('input');
-        answer.type = type;
-        answer.name = 'answer';
-        answer.classList.add('form-check-input', 'me-1');
-        answer.value = key;
-        answer.id = key;
-        if (type === 'radio') {
-            answer.required = true
-        }
-        label.textContent = value;
-        label.prepend(answer);
-        return label;
-    }
+	setRequired(form) {
+		const someChecked = !![...form.answer].some(input => input.checked);
+		!![...form.answer].filter(input => input.type !== 'radio').forEach(input => {
+			const required = someChecked ? '' : 'You must select at least one value'
+			input.setCustomValidity(required)
+		})
 
-    setRequired(form) {
-        const someChecked = !![...form.answer].some(input => input.checked);
-        !![...form.answer].filter(input => input.type !== 'radio').forEach(input => {
-            const required = someChecked ? '' : 'You must select at least one value'
-            input.setCustomValidity(required)
-        })
+	}
 
-    }
-
-    renderCard() {
-        return new Promise((resolve, reject) => {
-            const nextStep = () => {
-                const {
-                    STATE
-                } = this;
-                STATE.currentStep += 1;
-                const q = STATE.questions[STATE.currentStep - 1];
-                const isMmultiple = q.multiple_correct_answers === 'true';
-                let resultHtml = `
+	renderTemplate() {
+		const {
+			STATE
+		} = this;
+		const q = STATE.questions[STATE.currentStep - 1];
+		APP.innerHTML = `
                 <div class="container">
                 <h3 class="mt-4 mb-4 display-3">Quiz</h3>
                     <div class="card">
@@ -59,47 +49,70 @@ class QuizStepCard {
                         </form>
                     </div>
                 </div>
-                `
-                APP.innerHTML = resultHtml;
-                document.querySelector('.card-header').textContent = q.question;
-                const answers = document.createElement('ul');
-                answers.classList.add('answers', 'list-group');
-                Object.keys(q.answers).forEach(key => {
-                    if (!q.answers[key]) {
-                        return;
-                    }
-                    if (isMmultiple) {
-                        answers.append(this.addAnswerItem('checkbox', key, q.answers[key]));
-                    } else {
-                        answers.append(this.addAnswerItem('radio', key, q.answers[key]));
-                    }
-                })
-                const form = document.querySelector('form');
-                /** insert before button */
-                document.querySelector('button[type="submit"]').before(answers)
-                this.setRequired(form);
-                form.addEventListener('change', this.setRequired.bind(this, form))
-                form.addEventListener('submit', submitHandler);
+                `;
+		document.querySelector('.card-header').textContent = `Question ${STATE.currentStep} of ${STATE.questions.length}`;
+		document.querySelector('.card-title').textContent = q.question;
+		document.querySelector('.card-text').textContent = q.multiple_correct_answers ? 'Choose one or more answer options' : 'Choose one answer';
+		const answers = document.createElement('ul');
+		answers.classList.add('answers', 'list-group');
+		Object.keys(q.answers).forEach(key => {
+			if (!q.answers[key]) {
+				return;
+			}
+			if (q.multiple_correct_answers) {
+				answers.append(this.addAnswerItem('checkbox', key, q.answers[key]));
+			} else {
+				answers.append(this.addAnswerItem('radio', key, q.answers[key]));
+			}
+		})
+		/** insert before button */
+		document.querySelector('button[type="submit"]').before(answers)
+	}
 
-                function submitHandler(event) {
-                    event.preventDefault();
-                    const answer = !isMmultiple 
-                        ? event.target.elements.answer.value 
-                        : [...event.target.elements.answer].filter(input => input.checked).map(input => input.value).join();
-                    form.removeEventListener('submit', submitHandler);
-                    form.removeEventListener('change', this.setRequired);
-                    if (STATE.questions.length === STATE.currentStep) {
-                        STATE.answers[STATE.currentStep] = new Answer(STATE.questions[STATE.currentStep - 1], answer);
-                        resolve(STATE)
-                    } else {
-                        STATE.answers[STATE.currentStep] = new Answer(STATE.questions[STATE.currentStep - 1], answer);
-                        nextStep()
-                    }
-                }
-            }
-            nextStep()
-        })
-    }
+	async renderCard() {
+		return new Promise((resolve, reject) => {
+			const prevStep = () => {
+				const {
+					STATE
+				} = this;
+				STATE.currentStep -= 1;
+				const q = STATE.questions[STATE.currentStep - 1];
+				const isMultiple = q.multiple_correct_answers === 'true';
+				this.renderTemplate();
+
+			}
+			const nextStep = async () => {
+				const {
+					STATE
+				} = this;
+				STATE.currentStep += 1;
+				const q = STATE.questions[STATE.currentStep - 1];
+				const isMultiple = q.multiple_correct_answers === 'true';
+				await this.renderTemplate();
+				const form = document.querySelector('form');
+				this.setRequired(form);
+				form.addEventListener('change', this.setRequired.bind(this, form))
+				form.addEventListener('submit', submitHandler);
+
+				function submitHandler(event) {
+					event.preventDefault();
+					const answer = !isMultiple
+						? event.target.elements.answer.value
+						: [...event.target.elements.answer].filter(input => input.checked).map(input => input.value).join();
+					form.removeEventListener('submit', submitHandler);
+					form.removeEventListener('change', this.setRequired);
+					if (STATE.questions.length === STATE.currentStep) {
+						STATE.answers[STATE.currentStep] = new Answer(STATE.questions[STATE.currentStep - 1], answer);
+						resolve(STATE)
+					} else {
+						STATE.answers[STATE.currentStep] = new Answer(STATE.questions[STATE.currentStep - 1], answer);
+						nextStep()
+					}
+				}
+			}
+			nextStep()
+		})
+	}
 }
 
 export default QuizStepCard;
